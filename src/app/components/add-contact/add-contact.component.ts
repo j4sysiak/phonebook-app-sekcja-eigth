d69322e8fae5b88842bba09096d36755445f222e
Jacek <jaceksysiak@wp.pl>
2021-03-26 07:44:06 +0100

    26-03-2021 07:44

--- src/app/components/add-contact/add-contact.component.ts
+++ src/app/components/add-contact/add-contact.component.ts
@@ -12,7 +12,7 @@ import { Router } from '@angular/router';
 export class AddContactComponent implements OnInit {
 
   contact: Contact;
-
+/*&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&*/
   constructor(private service: PhonebookService,
               private router: Router) { }
 
